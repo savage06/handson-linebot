368754516ba6ad28447d04d93783b6fcddc16c46
savage06 <shuhei7114@gmail.com>
2020-02-20 16:50:56 +0900

    画像機能追加

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         message
-
+    )
 if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT", 5000))
