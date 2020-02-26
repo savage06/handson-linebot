a3cdc23ca39d760aad320b78f7c127c4b3c78826
savage06 <shuhei7114@gmail.com>
2020-02-26 22:36:42 +0900

    USED SQL

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
 from flask_sqlalchemy import SQLAlchemy
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -16,14 +17,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
+
+#モデル作成
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    usermessage = db.Column(db.String(80), unique=True)
-
-
+    usermessage = db.Column(db.String(80))
     def __init__(self, usermessage):
         self.usermessage = usermessage
-
 
 
 #環境変数取得
@@ -39,9 +39,12 @@ def record_message(userMessage):
     db.session.commit()
 
     all_messages = db.session.query(Message).all()
-    Message = all_messages[0].userMessage
-    return Message
-
+    messages = []
+    #LINEは一度に5つまでメッセージ送れる
+    for i in range(5):
+        message = all_messages[i].usermessage
+        messages.append(TextSendMessage(text=message))
+    return messages
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -63,10 +66,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = record_message(event.message.text)
+    messages = record_message(event.message.text)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=message))
+        messages)
 
 
 if __name__ == "__main__":
