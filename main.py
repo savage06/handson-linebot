8459b235769eb5528eae272b8b8c7a128a9a877d
savage06 <shuhei7114@gmail.com>
2020-02-26 18:00:32 +0900

    initial commit

--- main.py
+++ main.py
@@ -21,51 +21,6 @@ LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-# hands_to_int
-def hands_to_int(userhand):
-    if userhand == "グー":
-        return 0
-    elif userhand == "チョキ":
-        return 1
-    elif userhand == "パー":
-        return 2
-    else:
-        return -1
-
-def select_bothand():
-   return random.randint(0, 2)
-
-def judge(userhand, bothand):
-    if userhand == -1:
-        return "Select by グー/チョキー/パー"
-
-    if bothand == 0:
-        hand = "グー"
-    elif bothand == 1:
-        hand = "チョキ"
-    else:
-        hand = "パー"
- 
-    if (userhand - bothand + 3) % 3 == 0:
-        message = StickerSendMessage(
-            package_id = 1,
-            sticker_id = 1
-        )	    
-        return message
-    elif (userhand - bothand + 3) % 3 == 1:
-        message = AudioSendMessage(
-            original_content_url = "https://hanson1.herokuapp.com/static/audios/se_maoudamashii_onepoint33.mp3",
-            duration = 1000
-        )
-        return message
-    else:
-        message = ImageSendMessage(
-            original_content_url = "https://hanson1.herokuapp.com/static/images/index.jpeg", 
-            preview_image_url = "https://hanson1.herokuapp.com/static/images/index.jpeg"
-        )
-        return message
-    return TextSendMessage(text="bothand is " + hand + "\n" + text)
-
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -86,11 +41,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = judge(hands_to_int(event.message.text), select_bothand())
+    message = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
-        message
-    )
+        TextSendMessage(text=message))
+
 
 if __name__ == "__main__":
 #    app.run()
