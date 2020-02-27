40f16c1680c9a9028a0d35f0b3f4931a12fe9360
savage06 <shuhei7114@gmail.com>
2020-02-27 10:49:17 +0900

    test

--- main.py
+++ main.py
@@ -34,6 +34,14 @@ class Room(db.Model):
 #   return rooms(0)
 
 
+#環境変数取得
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -51,7 +59,7 @@ def show_room(userMessage):
     all_rooms = db.session.query(Room).all()
     print(all_rooms)
     return "show_room"
-    
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = show_room(event.message.text)
