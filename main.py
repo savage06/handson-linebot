5e9482d730d4670518e7f93b83be3895066c9112
savage06 <shuhei7114@gmail.com>
2020-02-27 12:17:27 +0900

    test

--- main.py
+++ main.py
@@ -26,10 +26,11 @@ class Room(db.Model):
     roomact = db.Column(db.Integer)
 
     def __init__(self, group_id,phase, player, roomact):
+        self.group_id = group_id
         self.phase = phase
         self.player = player
         self.roomact = roomact  
-        self.group_id = group_id
+        
     
 # def thisRoom():
 #   rooms = session.query(Room).filter(Room.id==event.source.group_id).all()
@@ -68,12 +69,24 @@ def record_room(event):
     groupId = event.source.group_id
     db.session.add(Room(groupId, "suspend", 0, 0))
     db.session.commit()
-    message = "グループID({})をDBに保存しました".format(groupId)
+    message = "ゲームを開始する場合は「開始」と入力してください"
+    return message
+
+def receive_message(event):
+    groupId = event.source.group_id
+    userMessage = event.message.text
+    if userMessage == "開始":
+        f = db.session.query(Room).filter(Room.group_id == groupId).first()
+        f.phase == "invite"
+        db.session.commit()
+        message = "参加者は「参加」と入力してください"
+    else: 
+        message = "不正な入力です"
     return message
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = show_room(event)
+    message = receive_message(event)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text = message))
